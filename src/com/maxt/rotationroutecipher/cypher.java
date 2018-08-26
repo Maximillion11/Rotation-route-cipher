0a19a29c7fa420c87844072a359dc982d5adcee8
Max Turnell <max.turnell@hotmail.co.uk>
2018-08-26 13:59:48 +0100

    Added: Constructor

--- src/com/maxt/rotationroutecipher/cypher.java
+++ src/com/maxt/rotationroutecipher/cypher.java
@@ -2,4 +2,9 @@ package com.maxt.rotationroutecipher;
 
 public class cypher {
 
+    public cypher (String inputText, int gridX, int gridY) {
+        int[] gridDimensions = { gridX, gridY };
+
+
+    }
 }
