e2786649ee8946c83b8743c0068012fce853167c
Max Turnell <max.turnell@hotmail.co.uk>
2018-08-27 10:21:02 +0100

    Added: Generate char grid Added: Loop for X and Y through the grid

--- src/com/maxt/rotationroutecipher/cypher.java
+++ src/com/maxt/rotationroutecipher/cypher.java
@@ -2,9 +2,24 @@ package com.maxt.rotationroutecipher;
 
 public class cypher {
 
-    public cypher (String inputText, int gridX, int gridY) {
+    public cypher (String inputText, int gridX, int gridY, boolean rotateClockwise) {
         int[] gridDimensions = { gridX, gridY };
+        char[] inputGrid = generateGrid(inputText, gridDimensions);
 
+        for (int x = 0; x < gridDimensions[0]; x++) {
+            for (int y = 0; y < gridDimensions[1]; y++) {
 
+            }
+        }
+    }
+
+    private char[] generateGrid (String inputText, int[] gridDimensions) {
+        char[] inputGrid = new char[gridDimensions[0] * gridDimensions[1]];
+
+        for (int i = 0; i < (gridDimensions[0] * gridDimensions[1]); i++) {
+            inputGrid[i] = inputText.charAt(i);
+        }
+
+        return inputGrid;
     }
 }
